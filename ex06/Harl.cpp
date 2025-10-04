28563327cce5c4aa85bb4bcdc45cdf4fcdb7b1e8
Ayoub Adouay <ayadouay@c4r8p9.1337.ma>
2025-10-04 15:17:54 +0100

    add

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -1,18 +1,22 @@
 #include "Harl.hpp"
 
 void Harl::debug(void) {
-    std::cout << "I love having extra bacon for my burger. I really do!" << std::endl;
+    std::cout << "[ DEBUG ]\n";
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void) {
-    std::cout << "I cannot believe adding extra bacon costs more money!" << std::endl;
+    std::cout << "[ INFO ]\n";
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void) {
-    std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+    std::cout << "[ WARNING ]\n";
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void) {
+    std::cout << "[ ERROR ]\n";
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -21,15 +25,14 @@ void    Harl::complain(std::string level){
     std::string chois[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
     int index = -1;
 
-    if (strcmp(level.c_str(), "DEBUG") == 0)
-        index = 0;
-    else if (strcmp(level.c_str(), "INFO") == 0)
-        index = 1;
-    else if (strcmp(level.c_str(), "WARNING") == 0)
-        index = 2;
-    else if (strcmp(level.c_str(), "ERROR") == 0)
-        index = 3;
-
+    for (size_t i = 0; i < 4; i++)
+    {
+        if(level == chois[i]){
+            index = i;
+            break ;
+        }
+    }
+    
     void    (Harl::*print_msg[4])(void) = {
         &Harl::debug,
         &Harl::info,
@@ -49,6 +52,6 @@ void    Harl::complain(std::string level){
             (this->*print_msg[3])();
             break ;
         default:
-            std::cout << "Unknown complaint level: " << level << std::endl;
+            std::cout << "[ Probably complaining about insignificant problems ]"<< std::endl;
     }
 }
